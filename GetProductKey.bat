51af4c839a2a5d13761968f9fb34b03d2c72e8e0
Apix0n <akselturcyilmaz@gmail.com>
2022-09-27 20:24:32 +0200

    The script now gets the registry license Also changed systeminfo to wmic and it now gets only edition and build number

--- GetProductKey.bat
+++ GetProductKey.bat
@@ -1,11 +1,24 @@
 @echo off
 
+:: Made by Apix
+:: https://github.com/Apix0n/GPK
+
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" && exit
+
+mkdir GPK_files
 mkdir tmpGPK_%computername%
+attrib +h tmpGPK_%computername%
 set dir=%~dp0%
 cd %dir%
 
-wmic path softwarelicensingservice get OA3xOriginalProductKey > .\tmpGPK_%computername%\productkey.txt
-systeminfo > .\tmpGPK_%computername%\systeminfo.txt
+:oem
+wmic path softwarelicensingservice get OA3xOriginalProductKey > .\tmpGPK_%computername%\pk-oem.txt
 
-powershell Compress-Archive .\tmpGPK_%computername%\* GPK_%computername%.zip
+:reg
+reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" /v BackupProductKeyDefault > .\tmpGPK_%computername%\pk-reg.txt
+
+:next
+wmic os get Caption, BuildNumber /value > .\tmpGPK_%computername%\ebn-info.txt
+powershell Compress-Archive -Force .\tmpGPK_%computername%\* .\GPK_files\GPK_%computername%.zip
 rmdir /s /q tmpGPK_%computername%
+exit
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # GetProductKey
+
+## Download link:
+
+https://apix0n.github.io/GPK or https://apix0n.github.io/GPK/GetProductKey.bat (same link)
+
+## What does the script do?
+
+The script creates a `GPK_%computername%.zip` archive in a `GPK_files` folder containing:
+
+* The OEM original license key, (in `pk-oem.txt`)
+    * Original license of computers that came with Windows 8+ pre-installed.
+* The license key in the Registry, (in `pk-reg.txt`)
+    * Actual license installed in the computer.
+* The OS edition and its Build Number (in `ebn-info.txt`)
+
+### Made by Apix with ❤️
